66f0e73ebcad88bb46c1a812a63144e88bb1d197
Nilesh Sah <nilesh.sah13@gmail.com>
2021-01-02 23:58:54 +0100

    Implement reverse with fold

--- src/main/scala/com/solutions/P05.scala
+++ src/main/scala/com/solutions/P05.scala
@@ -6,11 +6,16 @@ package com.solutions
 //    res0: List[Int] = List(8, 5, 3, 2, 1, 1)
 
 object P05 {
-  def reverse[A](as: List[A]): List[A] = {
+  def reverse[A](as: List[A]): List[A] = reverseWithFold(as)
+
+  def reverseRecursive[A](as: List[A]): List[A] = {
     def reverseR(rs: List[A], as: List[A]): List[A] = as match {
       case Nil => rs
       case h :: tail => reverseR(h :: rs, tail)
     }
     reverseR(List[A](), as)
   }
+
+  def reverseWithFold[A](as: List[A]): List[A] =
+    as.foldRight(List[A]()) { (left, cur) => cur ::: List(left)}
 }
