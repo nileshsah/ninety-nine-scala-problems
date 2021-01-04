3aede037e24dcec29cbfd975938512d54b4f921c
Nilesh Sah <nilesh.sah13@gmail.com>
2021-01-04 22:00:27 +0100

    Implement unfold left operation for range

--- src/main/scala/com/solutions/P22.scala
+++ src/main/scala/com/solutions/P22.scala
@@ -6,7 +6,17 @@ package com.solutions
 //    res0: List[Int] = List(4, 5, 6, 7, 8, 9)
 
 object P22 {
-  def range(start: Int, end: Int): List[Int] =
+  def range(start: Int, end: Int): List[Int] = rangeWithUnfold(start, end)
+
+  def rangeRecursive(start: Int, end: Int): List[Int] =
     if (start > end) Nil else start :: range(start + 1, end)
 
+  def unfoldLeft[A, B](s: B)(f: B => Option[(A, B)]): List[A] = f(s) match {
+    case None => Nil
+    case Some((a, b)) => unfoldLeft(b)(f) ::: List(a)
+  }
+
+  def rangeWithUnfold(start: Int, end: Int): List[Int] = unfoldLeft(end) {
+    e => if (e < start) Option.empty else Option((e, e - 1))
+  }
 }
